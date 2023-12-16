84ea21ebea1804096e1ee816661d881491a9ea06
Nestor Mamani <nmamanipantoja@gmail.com>
2023-12-16 07:41:10 -0500

    feat: countdown

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,61 @@
-import Link from "next/link";
+"use client";
+import dayjs from "dayjs";
+import Countdown from "react-countdown";
 
-import { CreatePost } from "@/app/_components/create-post";
-import { api } from "@/trpc/server";
+const END_DATE = new Date(2024, 4, 19, 0, 0, 0, 0);
+const GOAL = 3000;
+const current = 0;
+const TOTAL_WEEKS = 22;
 
-export default async function Home() {
-  const hello = await api.post.hello.query({ text: "from tRPC" });
-
+export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+          <span className="text-[hsl(280,100%,70%)]">{current}</span>/{GOAL}{" "}
+          <span className="text-6xl">
+            {Math.floor((current / GOAL) * 100)}%
+          </span>
         </h1>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
+          <div className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20">
+            <div className="flex flex-col items-center gap-2 text-lg">
+              <Countdown
+                date={END_DATE}
+                renderer={(props) => (
+                  <span className="text-2xl text-[hsl(280,100%,70%)]">
+                    Week{" "}
+                    {TOTAL_WEEKS -
+                      (props.days / 7 > 1
+                        ? Math.floor(props.days / 7)
+                        : 1)}{" "}
+                    (
+                    {Math.floor(
+                      ((TOTAL_WEEKS -
+                        (props.days / 7 > 1 ? Math.floor(props.days / 7) : 1)) /
+                        TOTAL_WEEKS) *
+                        100,
+                    )}
+                    %)
+                  </span>
+                )}
+              />
+              <div className="flex flex-wrap justify-center gap-2 text-4xl">
+                <Countdown
+                  date={END_DATE}
+                  renderer={(props) => <div>{props.days} days</div>}
+                />
+                <Countdown // end of today
+                  date={dayjs().endOf("day").toDate()}
+                  intervalDelay={0}
+                  precision={1}
+                  renderer={(props) => <div>{props.total}</div>}
+                />
+              </div>
             </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
+          </div>
         </div>
-        <div className="flex flex-col items-center gap-2">
-          <p className="text-2xl text-white">
-            {hello ? hello.greeting : "Loading tRPC query..."}
-          </p>
-        </div>
-
-        <CrudShowcase />
       </div>
     </main>
-  );
-}
-
-async function CrudShowcase() {
-  const latestPost = await api.post.getLatest.query();
-
-  return (
-    <div className="w-full max-w-xs">
-      {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}</p>
-      ) : (
-        <p>You have no posts yet.</p>
-      )}
-
-      <CreatePost />
-    </div>
   );
 }
