fc4e465d83c6b01211c8eec3bc1b78e1bacb3d0b
Nestor Mamani <nmamanipantoja@gmail.com>
2023-12-16 08:22:28 -0500

    feat: footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,11 +14,12 @@ const END_DATE = new Date(2024, 4, 19, 0, 0, 0, 0);
 const GOAL = 3000;
 const current = 0;
 const TOTAL_WEEKS = 22;
+const GITHUB_LINK = "https://github.com/nestorxyz/unique-countdown";
 
 export default function Home() {
   return (
     <main
-      className={`${silk.variable} font-silk relative flex min-h-screen flex-col items-center justify-center text-white`}
+      className={`${silk.variable} relative flex min-h-screen flex-col items-center justify-center font-silk text-white`}
     >
       <img
         src="/bg.png"
@@ -72,6 +73,34 @@ export default function Home() {
           </div>
         </div>
       </div>
+      <footer className="absolute bottom-4 z-20">
+        <div className="flex flex-col items-center justify-center gap-4 p-4">
+          <div className="group relative flex gap-2 overflow-hidden rounded-full p-2 px-4 hover:bg-white/10">
+            <a
+              href={GITHUB_LINK}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="z-30 flex items-center gap-2"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+                <path d="M9 18c-4.51 2-5-2-7-2" />
+              </svg>
+              <div className="text-white">Github</div>
+            </a>
+          </div>
+        </div>
+      </footer>
     </main>
   );
 }
